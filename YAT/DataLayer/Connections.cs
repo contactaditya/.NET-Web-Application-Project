b5396c27101cd1fb748215dc5751d1dadf30e1af
psultan <paul.sultan@gmail.com>
2015-04-15 15:19:29 +0000

    Connections.cs edited online with Bitbucket

--- YAT/DataLayer/Connections.cs
+++ YAT/DataLayer/Connections.cs
@@ -12,25 +12,8 @@ namespace DataLayer
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
-        /*
-        //paul - i think it might have to be closer to this
-        [Required]
-        public int FromId { get; set; } 
-        [ForeignKey("FromId")] 
-        public User From { get; set; } 
-        
-        [Required]
-        public int ToId { get; set; } 
-        [ForeignKey("ToId")] 
-        public User To { get; set; } 
-        */
-        
-        [Required]
-        [ForeignKey("Id")]
+               
         public virtual User From { get; set; }
-        [Required]
-        [ForeignKey("Id")]
         public virtual User To { get; set; }
         
         public bool IsRemoved { get; set; }
