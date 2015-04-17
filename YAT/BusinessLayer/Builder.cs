5b528e461c9411c86730ab81381085e85eefaa0d
Arik Dexler <arikdexler@yahoo.com>
2015-04-17 17:46:51 -0400

    Added queryUsers

--- YAT/BusinessLayer/Builder.cs
+++ YAT/BusinessLayer/Builder.cs
@@ -88,7 +88,10 @@ namespace BusinessLayer
                 paul.Likes.Add(nemo);
                 sue.Likes.Add(avengers);
                 sue.Likes.Add(gump);
-                Ariel.Likes.Add(nemo);
+
+                Likes movie = dbContext.Likes.FirstOrDefault(L => L.Movie == "Avengers");
+                Debug.Print(movie.Movie + " " + movie.Id);
+                Ariel.Likes.Add(movie);
 
                 var sueConnection = new Connections { From = sue, To = mike, IsBlocked = true, IsRemoved=false };
                 var sueConnection2 = new Connections { From = sue, To = paul, IsBlocked=false, IsRemoved = true };
@@ -120,6 +123,30 @@ namespace BusinessLayer
                 }
             }
         }
+
+        public List<User> queryUsers(int minAge, int maxAge, bool gender, int zipcode, int SearcherID)
+        {
+            //get the user's likes list, count the likes match for every result, and sort by top match
+            using (var dbContext = new YATContext())
+            {
+              
+                List<User> users = dbContext.User.SqlQuery("Select * from dbo.Users inner join " +
+                 " (select filteredUsers.ID,count(filteredUsers.ID) as score   from " +
+                "(SELECT u.ID from dbo.Users as u where " +
+                 "u.ID!=" + SearcherID + " and u.Age >=" + minAge + " and u.Age<=" + maxAge + " and u.Gender = " + Convert.ToInt32(gender) + "  and u.Zip = " + zipcode + ")" +
+                  " as filteredUsers " +
+                          "left join" +
+                 "(SELECT dbo.LikesUsers.user_ID, dbo.LikesUsers.Likes_Id from   dbo.LikesUsers where " +
+                        "dbo.LikesUsers.Likes_Id in (SELECT dbo.LikesUsers.Likes_ID from   dbo.LikesUsers where " +
+                                                      " dbo.LikesUsers.User_ID=1)) as movies " +
+                                                            " on filteredUsers.ID = movies.User_Id " +
+                                                                  " group by filteredUsers.ID) as results on results.ID=dbo.Users.ID order by score").ToList();
+      
+                return users;
+            }
+        }
+             
+
     }
 
     
--- YAT/ConsoleApplication1/Program.cs
+++ YAT/ConsoleApplication1/Program.cs
@@ -5,14 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using BusinessLayer;
 using System.Data.Entity;
+using DataLayer;
+using System.Diagnostics;
 namespace ConsoleApplication1
 {
     class Program
     {
+    
+      
+
         static void Main(string[] args)
         {
-                Builder b = new Builder();
-                b.putData();
+            //using (var dbContext = new YATContext())
+            //{
+            //    User Ariel = dbContext.User.FirstOrDefault(u => u.FirstName == "DEX");
+            //    Likes movie = dbContext.Likes.FirstOrDefault(L => L.Movie == "Avengers");
+            //    Console.WriteLine(movie.Movie + " " + movie.Id);
+            //    Ariel.Likes.Add(movie);
+            //    dbContext.SaveChanges();
+            //}
+
+
+            //Work on this
+            Builder builder = new Builder();
+            string test = builder.queryUsersTEXT(minAge: 20, maxAge: 30, gender: false, zipcode: 11791, SearcherID: 1);
+            Console.WriteLine(test);
+            Debug.WriteLine(test);
+
+
+            List<User> users = builder.queryUsers(minAge: 20, maxAge: 30, gender: false, zipcode: 11791, SearcherID: 1);
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.FirstName + " " + user.LastName);
+
+            }
+
                 Console.WriteLine("Done!");
                 Console.ReadKey();
         }
