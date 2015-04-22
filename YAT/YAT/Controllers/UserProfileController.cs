3d57a913b69bbb0ec6163f78032092aa0cdb77fa
contactaditya <contactadityag@gmail.com>
2015-04-22 16:18:31 -0400

    This is the new user profile that I have started work on.

---
+++ YAT/YAT/Controllers/UserProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YAT.Controllers
+{
+    public class UserProfileController : Controller
+    { 
+        // 
+        // GET: UserProfile
+ 
+        public ActionResult UserProfile() 
+        { 
+            return View(); 
+        } 
+ 
+        // 
+        // GET: UserProfile/Welcome
+ 
+        public ActionResult Welcome() 
+        { 
+            return View(); 
+        } 
+    } 
+}
+
