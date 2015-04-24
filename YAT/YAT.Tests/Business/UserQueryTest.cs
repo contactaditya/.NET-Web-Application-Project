9596197d4262f4f66b67887bbc141a53c8d4a10a
Arik Dexler <arikdexler@yahoo.com>
2015-04-24 01:01:47 -0400

    Added test cases

---
+++ YAT/YAT.Tests/Business/UserQueryTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YAT;
+using YAT.Controllers;
+using BusinessLayer;
+using DataLayer;
+namespace YAT.Tests.Business
+{
+    [TestClass]
+    public class UserQueryTest
+    {
+        Builder b = new Builder();
+  
+        [TestMethod]
+        public void qrySortByMatch ()    
+        {
+            List<User> users = qryTester(UserSort.Match);
+            // Assert
+          Assert.IsNotNull(users);
+        }
+
+        [TestMethod]
+        public void qrySortByJoinDate()
+        {
+            List<User> users = qryTester(UserSort.LastJoin);
+            Assert.IsNotNull(users);
+        }
+
+        [TestMethod]
+        public   void qrySortByLogDate()
+        {
+            List<User> users = qryTester(UserSort.LastLog);
+            Assert.IsNotNull(users);
+        }
+
+
+        public List<User> qryTester(UserSort sortby)
+        {
+
+            Builder b = new Builder();
+
+            //Test searching users
+            List<User> users = b.queryUsers(minAge: 20, maxAge: 30, gender: false, zipcode: 11791, SearcherID: 1, sortBy: sortby);
+            return users;
+
+        }
+
+    }
+}
